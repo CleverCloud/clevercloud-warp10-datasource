ef65f3fff0e625e1f686bdff1f2bf66be3254128
Clara Layus <clara.layus@clever-cloud.com>
2023-06-28 17:51:17 +0200

    Implementation function testdatabase update

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,22 +1,79 @@
-import {CoreApp, DataSourceInstanceSettings} from '@grafana/data';
-import {DataSourceWithBackend, TestingStatus} from '@grafana/runtime';
+import {DataSourceInstanceSettings,} from '@grafana/data';
+import {DataSourceWithBackend, getBackendSrv, TestingStatus} from '@grafana/runtime';
+import {lastValueFrom} from 'rxjs';
 
-import {DEFAULT_QUERY, MyDataSourceOptions, MyQuery} from './types';
+import {WarpDataSourceOptions, WarpQuery} from './types';
 
-export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
-  constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
+
+export class DataSource extends DataSourceWithBackend<WarpQuery, WarpDataSourceOptions> {
+
+  //Information database
+  private path: string
+
+  /*private access: string
+  private const: ConstProp[]*/
+
+  /**
+   * @param instanceSettings
+   */
+  constructor(instanceSettings: DataSourceInstanceSettings<WarpDataSourceOptions>) {
     super(instanceSettings);
+    this.path = instanceSettings.jsonData.path ?? ""
+    /*this.access = instanceSettings.jsonData.access ?? ""
+    this.const = instanceSettings.jsonData.const ?? []*/
   }
 
+  /**
+   * used by datasource configuration page to make sure the connection is working
+   * @return {Promise<any>} Response
+   */
   async testDatasource(): Promise<any> {
+
+    let message = ""
+    let status = ""
+
+    const query = lastValueFrom(this.doRequest({refId: "", queryText: "1 2 +"}))
+
+    await query.then((value) => {
+        if (value.status === 200) {
+          message = "Datasource is working"
+          status = "Ok"
+        } else {
+          message = "An error has occurred"
+          status = "Error"
+        }
+      }
+    ).catch((value) => {
+      message = "An error has occurred"
+      status = "Error"
+    })
+
     return {
-      message: "I'm alive",
+      message: message,
       details: undefined,
-      status: "Ok"
+      status: status
     } as TestingStatus
   }
 
-  getDefaultQuery(_: CoreApp): Partial<MyQuery> {
-    return DEFAULT_QUERY
+  /*query(request: DataQueryRequest<MyQuery>): Observable<DataQueryResponse> {
+
+  }*/
+
+  /**
+   * send request to Warp10
+   * @param query
+   * @return {Observable<FetchResponse<unknown>>} Response
+   */
+  doRequest(query: WarpQuery) {
+    return getBackendSrv().fetch(
+      {
+        url: this.path + "/api/v0/exec",
+        method: "POST",
+        data: query.queryText,
+        headers: [['Accept', "undefined"], ['Content-Type', 'text/plain; charset=UTF-8']]
+      }
+    )
   }
+
+
 }
--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,9 @@
-import { DataSourcePlugin } from '@grafana/data';
-import { DataSource } from './datasource';
-import { ConfigEditor } from './components/ConfigEditor';
-import { QueryEditor } from './components/QueryEditor';
-import { MyQuery, MyDataSourceOptions } from './types';
+import {DataSourcePlugin} from '@grafana/data';
+import {DataSource} from './datasource';
+import {ConfigEditor} from './components/ConfigEditor';
+import {QueryEditor} from './components/QueryEditor';
+import {WarpQuery, WarpDataSourceOptions} from './types';
 
-export const plugin = new DataSourcePlugin<DataSource, MyQuery, MyDataSourceOptions>(DataSource)
+export const plugin = new DataSourcePlugin<DataSource, WarpQuery, WarpDataSourceOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
   .setQueryEditor(QueryEditor);
