ca4dca1c02179f8f6d49af87bbc25fb00bc79e38
AlexandreBocquier <alexandre.bocquier@clever-cloud.com>
2025-04-18 15:41:52 +0200

    test(backend): add parsing functions tests

--- go.mod
+++ go.mod
@@ -9,19 +9,33 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/apache/arrow-go/v18 v18.0.1-0.20241212180703-82be143d7c30 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cheekybits/genny v1.0.0 // indirect
 	github.com/chromedp/cdproto v0.0.0-20220208224320-6efb837e6bc2 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/containerd/platforms v0.2.1 // indirect
+	github.com/cpuguy83/dockercfg v0.3.2 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/docker v28.0.1+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/ebitengine/purego v0.8.2 // indirect
 	github.com/elazarl/goproxy v1.7.1 // indirect
 	github.com/fatih/color v1.15.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/getkin/kin-openapi v0.129.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
@@ -43,51 +57,74 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magefile/mage v1.15.0 // indirect
+	github.com/magiconair/properties v1.8.9 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattetti/filebuffer v1.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/patternmatcher v0.6.0 // indirect
+	github.com/moby/sys/sequential v0.5.0 // indirect
+	github.com/moby/sys/user v0.1.0 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/oasdiff/yaml v0.0.0-20241210131133-6b86fb107d80 // indirect
 	github.com/oasdiff/yaml3 v0.0.0-20241210130736-a94c01f36349 // indirect
 	github.com/oklog/run v1.1.0 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/prometheus/client_golang v1.20.5 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shirou/gopsutil/v4 v4.25.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	github.com/testcontainers/testcontainers-go v0.36.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/unknwon/bra v0.0.0-20200517080246-1e3013ecaff8 // indirect
 	github.com/unknwon/com v1.0.1 // indirect
 	github.com/unknwon/log v0.0.0-20150304194804-e617c87089d3 // indirect
 	github.com/urfave/cli v1.22.16 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.59.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace v0.59.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.59.0 // indirect
 	go.opentelemetry.io/contrib/propagators/jaeger v1.34.0 // indirect
 	go.opentelemetry.io/contrib/samplers/jaegerremote v0.28.0 // indirect
-	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.34.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.34.0 // indirect
-	go.opentelemetry.io/otel/metric v1.34.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.34.0 // indirect
-	go.opentelemetry.io/otel/trace v1.34.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
 	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/tools v0.28.0 // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
---
+++ pkg/plugin/warp10_testcontainer_test.go
@@ -0,0 +1,238 @@
+package plugin
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	l "github.com/grafana/grafana-plugin-sdk-go/backend/log"
+	b "github.com/miton18/go-warp10/base"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+var client *b.Client
+var ds Datasource
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "warp10io/warp10:3.4.1-alpine",
+		ExposedPorts: []string{"8080/tcp", "8081/tcp"},
+		WaitingFor:   wait.ForListeningPort("8080/tcp"),
+	}
+	warpContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	logger := l.New()
+	port, _ := warpContainer.Ports(ctx)
+	logger.Info(fmt.Sprintf("ports: %s", port["8080/tcp"][0].HostPort))
+
+	if len(port["8080/tcp"]) < 1 {
+		log.Fatalf("Failed to get 8080/tcp port")
+	}
+	warpPort := port["8080/tcp"][0].HostPort
+	client = b.NewClient(fmt.Sprintf("http://localhost:%v", warpPort))
+	ds = Datasource{client}
+
+	exitVal := m.Run()
+
+	if err := testcontainers.TerminateContainer(warpContainer); err != nil {
+		log.Fatalf("Failed to terminate container: %s", err)
+	}
+
+	os.Exit(exitVal)
+}
+
+func TestTableResultQuery(t *testing.T) {
+	wsQuery := `{
+		"expr": "{ 'columns' [ { 'text' 'columnA' 'type' 'number' 'sort' true 'desc' true } { 'text' 'columnB' 'type' 'number' } ] 'rows' [ [ 10 20 ] [ 100 200 ] [ 100 200 ] [ 100 200 ] [ 100 200 ] [ 100 200 ] [ 100 200 ] [ 100 200 ] ] }"
+	}`
+
+	var jsonRawMsg json.RawMessage
+	if err := json.Unmarshal([]byte(wsQuery), &jsonRawMsg); err != nil {
+		t.Errorf("Failed to parse JSON: %v", err)
+	}
+
+	queryDataRes, err := ds.QueryData(context.Background(), &backend.QueryDataRequest{
+		PluginContext: backend.PluginContext{},
+		Headers:       nil,
+		Queries: []backend.DataQuery{
+			{
+				RefID:         "A",
+				QueryType:     "",
+				MaxDataPoints: 0,
+				Interval:      0,
+				TimeRange: backend.TimeRange{
+					From: time.Now().Add(-time.Hour),
+					To:   time.Now(),
+				},
+				JSON: jsonRawMsg,
+			},
+		},
+	})
+
+	if err != nil {
+		t.Errorf("Failed to execute query: %v", err)
+	}
+
+	// the value .results.A.frames[0].refId = "A" was removed from shoul be value, Grafana may add it after making a request to proxy
+	responseShouldBe := `{"results":{"A":{"status":200,"frames":[{"schema":{"name":"tableResults","fields":[{"name":"columnA","type":"number","typeInfo":{"frame":"float64","nullable":true}},{"name":"columnB","type":"number","typeInfo":{"frame":"float64","nullable":true}}]},"data":{"values":[[10,100,100,100,100,100,100,100],[20,200,200,200,200,200,200,200]]}}]}}}`
+	jsonResponse, err := queryDataRes.MarshalJSON()
+
+	if err != nil {
+		t.Errorf("Failed to marshal JSON: %v", err)
+	}
+
+	if !bytes.Equal([]byte(responseShouldBe), jsonResponse) {
+		t.Errorf("Response does not match expected output")
+	}
+
+}
+
+func TestGTSListQuery(t *testing.T) {
+	wsQuery := `{
+		"expr": "[ { 'c' 'testClass' 'l' {} 'a' {} 'v' [ [ 1619784000000000 42.5 ] [ 1619784001000000 43.2 ] ] } ]"
+	}`
+
+	var jsonRawMsg json.RawMessage
+	if err := json.Unmarshal([]byte(wsQuery), &jsonRawMsg); err != nil {
+		t.Errorf("Failed to parse JSON: %v", err)
+	}
+
+	queryDataRes, err := ds.QueryData(context.Background(), &backend.QueryDataRequest{
+		PluginContext: backend.PluginContext{},
+		Headers:       nil,
+		Queries: []backend.DataQuery{
+			{
+				RefID:         "A",
+				QueryType:     "",
+				MaxDataPoints: 0,
+				Interval:      0,
+				TimeRange: backend.TimeRange{
+					From: time.Now().Add(-time.Hour),
+					To:   time.Now(),
+				},
+				JSON: jsonRawMsg,
+			},
+		},
+	})
+
+	if err != nil {
+		t.Errorf("Failed to execute query: %v", err)
+	}
+
+	// the value .results.A.frames[0].refId = "A" was removed from shoul be value, Grafana may add it after making a request to proxy
+	responseShouldBe := `{"results":{"A":{"status":200,"frames":[{"schema":{"name":"testClass","fields":[{"name":"time","type":"time","typeInfo":{"frame":"time.Time"}},{"name":"testClass","type":"number","typeInfo":{"frame":"float64"}}]},"data":{"values":[[1619784000000,1619784001000],[42.5,43.2]]}}]}}}`
+	jsonResponse, err := queryDataRes.MarshalJSON()
+
+	if err != nil {
+		t.Errorf("Failed to marshal JSON: %v", err)
+	}
+
+	if !bytes.Equal([]byte(responseShouldBe), jsonResponse) {
+		t.Errorf("Response does not match expected output")
+	}
+}
+
+func TestArrayQuery(t *testing.T) {
+	wsQuery := `{
+		"expr": "[ 42.5 43.2 44.1 ]"
+	}`
+
+	var jsonRawMsg json.RawMessage
+	if err := json.Unmarshal([]byte(wsQuery), &jsonRawMsg); err != nil {
+		t.Errorf("Failed to parse JSON: %v", err)
+	}
+
+	queryDataRes, err := ds.QueryData(context.Background(), &backend.QueryDataRequest{
+		PluginContext: backend.PluginContext{},
+		Headers:       nil,
+		Queries: []backend.DataQuery{
+			{
+				RefID:         "A",
+				QueryType:     "",
+				MaxDataPoints: 0,
+				Interval:      0,
+				TimeRange: backend.TimeRange{
+					From: time.Now().Add(-time.Hour),
+					To:   time.Now(),
+				},
+				JSON: jsonRawMsg,
+			},
+		},
+	})
+
+	if err != nil {
+		t.Errorf("Failed to execute query: %v", err)
+	}
+
+	// the value .results.A.frames[0].refId = "A" was removed from shoul be value, Grafana may add it after making a request to proxy
+	responseShouldBe := `{"results":{"A":{"status":200,"frames":[{"schema":{"name":"arrayResults","fields":[{"name":"array_value","type":"number","typeInfo":{"frame":"float64","nullable":true}}]},"data":{"values":[[42.5,43.2,44.1]]}}]}}}`
+	jsonResponse, err := queryDataRes.MarshalJSON()
+
+	if err != nil {
+		t.Errorf("Failed to marshal JSON: %v", err)
+	}
+
+	if !bytes.Equal([]byte(responseShouldBe), jsonResponse) {
+		t.Errorf("Response does not match expected output")
+	}
+}
+
+func TestScalarQuery(t *testing.T) {
+	wsQuery := `{
+		"expr": "42"
+	}`
+
+	var jsonRawMsg json.RawMessage
+	if err := json.Unmarshal([]byte(wsQuery), &jsonRawMsg); err != nil {
+		t.Errorf("Failed to parse JSON: %v", err)
+	}
+
+	queryDataRes, err := ds.QueryData(context.Background(), &backend.QueryDataRequest{
+		PluginContext: backend.PluginContext{},
+		Headers:       nil,
+		Queries: []backend.DataQuery{
+			{
+				RefID:         "A",
+				QueryType:     "",
+				MaxDataPoints: 0,
+				Interval:      0,
+				TimeRange: backend.TimeRange{
+					From: time.Now().Add(-time.Hour),
+					To:   time.Now(),
+				},
+				JSON: jsonRawMsg,
+			},
+		},
+	})
+
+	if err != nil {
+		t.Errorf("Failed to execute query: %v", err)
+	}
+
+	// the value .results.A.frames[0].refId = "A" was removed from shoul be value, Grafana may add it after making a request to proxy
+	responseShouldBe := `{"results":{"A":{"status":200,"frames":[{"schema":{"name":"scalarResult","fields":[{"name":"scalar_value_float64","type":"number","typeInfo":{"frame":"float64"}}]},"data":{"values":[[42]]}}]}}}`
+	jsonResponse, err := queryDataRes.MarshalJSON()
+
+	if err != nil {
+		t.Errorf("Failed to marshal JSON: %v", err)
+	}
+
+	if !bytes.Equal([]byte(responseShouldBe), jsonResponse) {
+		t.Errorf("Response does not match expected output")
+	}
+}
